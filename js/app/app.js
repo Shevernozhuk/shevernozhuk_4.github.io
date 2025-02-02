6508c2ba496c5277b99ec57bfa3b17f781da3c52
Daniil <daniilsevernozuk@gmail.com>
2025-02-02 23:02:09 +0200

    day third

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,7 @@
 import { router } from './router.js';
+import { header } from './widgets/header.js';
 import { msg } from './widgets/msg.js';
+import { popup } from './widgets/popup.js';
 
 document.addEventListener('DOMContentLoaded', function(){
     const main = {
@@ -96,6 +98,8 @@ document.addEventListener('DOMContentLoaded', function(){
     };
 
     var app = Vue.createApp(main)
+    .component('Header',header)
+    .component('popup',popup)
     .component('msg',msg)
     .use(router)
     .mount('#content')
